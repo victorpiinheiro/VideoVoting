7180f30885a393151c5a9771aa76ef9c46979cd8
Victor Pinheiro <contato.vspinheiro@gmail.com>
2025-03-18 12:59:30 -0300

    ajustando componente loader para preencher tela cheia

--- frontend/youtube/src/components/Loader/styled.jsx
+++ frontend/youtube/src/components/Loader/styled.jsx
@@ -1,38 +1,38 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-width: 100vw;
-height: 100vh;
-background: #292929;
-position: fixed;
-opacity: .9;
-display: flex;
-align-items: center;
-justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background: #292929;
+  position: fixed;
+  top: 0;
+  left: 0;
+  opacity: 0.9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
 
-`
 export const Loading = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 10px solid white;
+  border-radius: 50%;
+  border-top-color: #c3a9f3;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  animation: rotate 1s infinite linear;
+  box-sizing: border-box;
 
-width: 100px;
-height: 100px;
-border: 10px solid white;
-border-radius: 50%;
-border-top-color: #c3a9f3;
-position: absolute;
-display: flex;
-align-items: center;
-justify-content: center;
-text-align: center;
-animation: rotate 1s infinite linear;
-
-
-
-
-@keyframes rotate {
-  100%{
-    transform: rotate(360deg) ;
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
   }
-
-}
-`
-
+`;
