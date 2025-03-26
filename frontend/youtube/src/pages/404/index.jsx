24cce1f3c503a3b6d4df5c695058000d9869dc8d
Victor Pinheiro <contato.vspinheiro@gmail.com>
2025-03-26 12:19:15 -0300

    Estilizando pagina 404

--- frontend/youtube/src/pages/404/index.jsx
+++ frontend/youtube/src/pages/404/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import { Container } from './styled';
 
 export default function Page404() {
   return (
-    <>
-      <h1>ERROR 404!</h1>
-    </>
+    <Container>
+      <h1>!ERROR 404!</h1>
+      <p>Página nao encontrada</p>
+
+     <Link to='/'>Voltar ao inicio</Link>
+    </Container>
+
   )
 }
---
+++ frontend/youtube/src/pages/404/styled.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+h1{
+  font-size: 60px;
+}
+
+p{
+  font-size: 24px;
+}
+
+a {
+  margin-top: 10px;
+  background: red;
+  padding: 10px 30px;
+  color: #f3f3f3;
+  border-radius: 5px;
+}
+
+`
